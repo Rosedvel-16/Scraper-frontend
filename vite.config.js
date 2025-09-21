11b07ff5684f84a7e107db2d90a5290c74ce86de
rosedvel <ros19ed@gmail.com>
2025-09-21 01:18:10 -0500

    pruebas

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://scraper-alquileres-backend.vercel.app/',
+        target: 'https://scraper-backend-xiwk.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
