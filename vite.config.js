a399a09fd1f221807bf80f6a70373290eaf486f0
rosedvel <ros19ed@gmail.com>
2025-09-22 20:21:19 -0500

    actualizacion

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://scraper-backend.vercel.app/',
+        target: 'https://scraper-backend-plum.vercel.app/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
